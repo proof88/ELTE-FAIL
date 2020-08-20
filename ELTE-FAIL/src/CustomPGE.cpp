a7e7524c9c9d9ae5508d1a4b9d389fa8ce6f7876
proof88 <pr0o0o0o0o0o0o0o0o0o0of88@gmail.com>
2020-08-20 15:04:39 +0200

    ...

--- ELTE-FAIL/src/CustomPGE.cpp
+++ ELTE-FAIL/src/CustomPGE.cpp
@@ -79,6 +79,7 @@ void CustomPGE::onGameInitialized()
     box1 = getPRRE().getObject3DManager().createFromFile("gamedata\\models\\cube.obj");
     box1->SetVertexTransferMode(PRRE_VT_DYN_DIR_1_BY_1);
     box1->getPosVec().SetZ(3); 
+    
 
     /*       
     PRREObject3D* const plane1 = getPRRE().getObject3DManager().createPlane(2, 2);
@@ -92,7 +93,8 @@ void CustomPGE::onGameInitialized()
     
     snail->SetScaling(0.02f);
     snail->getPosVec().SetZ(2);     
-    
+
+
     PRREObject3D* snail_lm = getPRRE().getObject3DManager().createFromFile("gamedata\\models\\snail_proofps\\snail_lm.obj");
     snail_lm->SetScaling(0.02f);
     snail_lm->Hide();
@@ -121,13 +123,16 @@ void CustomPGE::onGameInitialized()
     snail->SetVertexTransferMode(PRRE_VT_DYN_IND_SVA_GEN);
     snail->SetDoubleSided(true);
 
+
     // at this point, we should be safe to delete snail_lm since object's dtor calls material's dtor which doesn't free up the textures
     // however, a mechanism is needed to be implemented to correctly handle this situation.
     // WA1: CopyFromMaterial() should hardcopy the textures also; deleting material should delete its textures too;
     // WA2: (better) textures should maintain refcount. Material deletion would decrement refcount and would effectively delete textures when refcount reaches 0.
     delete snail_lm;
     snail_lm = NULL;
-    
+        /*
+    snail->SetVertexTransferMode(PRRE_VT_DYN_DIR_1_BY_1);
+          */
     
     
 
