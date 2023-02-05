945c192662ab7c23482b2c6e7ba0750bd0dffb31
proof88 <pr0o0o0o0o0o0o0o0o0o0of88@gmail.com>
2023-02-05 11:59:35 +0100

    RFR: delete gyorsan.txt, as user config file is used now

--- ELTE-FAIL/src/CustomPGE.cpp
+++ ELTE-FAIL/src/CustomPGE.cpp
@@ -20,6 +20,9 @@
 #include "../../../CConsole/CConsole/src/CConsole.h"
 
 
+static constexpr char* CVAR_CL_SERVER_IP = "cl_server_ip";
+
+
 // ############################### PUBLIC ################################
 
 
@@ -277,38 +280,12 @@ void CustomPGE::onGameInitialized()
         getNetwork().getClient().getBlackListedAppMessages().insert(static_cast<pge_network::TPgeMsgAppMsgId>(elte_fail::MsgUserCmdMove::id));
 
         std::string sIp = "127.0.0.1";
-        std::ifstream f;
-        f.open("gyorsan.txt", std::ifstream::in);
-        if (!f.good())
+        if (!getConfigProfiles().getVars()[CVAR_CL_SERVER_IP].getAsString().empty())
         {
-            getConsole().OLn("No gyorsan.txt found!");
+            sIp = getConfigProfiles().getVars()[CVAR_CL_SERVER_IP].getAsString();
+            getConsole().OLn("IP from config: %s", sIp.c_str());
         }
-        else
-        {
-            getConsole().OLn("Found gyorsan.txt");
-            const std::streamsize nBuffSize = 1024;
-            char cLine[nBuffSize];
-            int i = 0;
-            while (!f.eof())
-            {
-                f.getline(cLine, nBuffSize);
-                // TODO: we should finally have a strClr() version for std::string or FINALLY UPGRADE TO NEWER CPP THAT MAYBE HAS THIS FUNCTIONALITY!!!
-                PFL::strClrLeads(cLine);
-                const std::string sTrimmedLine(cLine);
-                if (i == 0)
-                {
-                    sIp = sTrimmedLine;
-                    getConsole().OLn("IP override: %s", sIp.c_str());
-                }
-                else
-                {
-                    getConsole().OLn("Logging override: %s", sTrimmedLine.c_str());
-                    getConsole().SetLoggingState(sTrimmedLine.c_str(), true);
-                }
-                i++;
-            };
-            f.close();
-        }
+        // TODO: log level override support: getConsole().SetLoggingState(sTrimmedLine.c_str(), true);
 
         if (!getNetwork().getClient().connectToServer(sIp))
         {
